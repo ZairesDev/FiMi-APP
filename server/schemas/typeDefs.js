b51e720b26b48080350d5fb0e9831f0a7dc2b39d
Chidori <business.faissal@outlook.com>
2022-04-07 19:39:54 -0500

    changed syntax verbage

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,7 +9,7 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    addComment(commentText: String!): Thought
+    addComment(commentText: String!): Comment
   }
 `;
 
