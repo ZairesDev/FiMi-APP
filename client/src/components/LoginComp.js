e7c139223d7a5ce650969e14cf654d13d66368b3
906284 <michaelcounseller@gmail.com>
2022-04-09 17:12:22 -0500

    added sign up css

--- client/src/components/LoginComp.js
+++ client/src/components/LoginComp.js
@@ -1,28 +1,22 @@
 import React, { useState } from 'react'
-
-
-import { Box, Button, Paper, TextField } from '@mui/material';
-
-
+import { Box, Button, Paper, TextField, Typography } from '@mui/material';
+import './LoginComp.css'
 
 const LoginComp = () => {
     return (
         <>
-            <Paper elevation={3}>
-                <Box
-                    component="form"
-                    sx={{
-                            mt: '50vh', width: '25%',
-                    }}
-                noValidate
-                autoComplete="on"
-                >
-
-                <Button variant="contained">Contained</Button>
-                <TextField id="standard-basic" label='Username' variant='standard' />
-                <TextField id="standard-basic" label="Password" variant="standard" />
-            </Box>
-        </Paper>
+            <Paper elevation={24} sx={{
+                    display: 'flex', flexWarp: 'wrap', justifyContent: 'center', mt: '30vh', width: '20vw'
+                }}>
+                <Box sx={{ display: 'flex', justifyContent: 'center', flexWarp: 'wrap', flexDirection: 'column', p: '50px', width: '15vw' }}> 
+                    <Typography className='form-spacing form-header'>
+                        Sign Up
+                    </Typography>
+                    <TextField className='form-spacing' id="standard-basic" label='Username' variant='standard' />
+                    <TextField className='form-spacing' id="standard-basic" label="Password" variant="standard" />
+                    <Button className='submit form-spacing' variant="contained">Submit</Button>
+                </Box>
+            </Paper>
         </>
     )
 
