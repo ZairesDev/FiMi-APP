38586c5c7ca3a21c18e6cba515aa70791efc0706
MichValenz <michvalenz27@gmail.com>
2022-04-12 21:06:52 -0500

    updated root json. completed server.js

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
-
 const { ApolloServer } = require("apollo-server-express");
-const { authMiddleware } = require("./utils/auth");
+const path = require("path");
 
+const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
 
@@ -19,11 +19,7 @@ const startServer = async () => {
 
 
   await server.start();
-
-  
   server.applyMiddleware({ app });
-
-  
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 };
 
@@ -32,6 +28,15 @@ startServer();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
 
 db.once("open", () => {
   app.listen(PORT, () => {
