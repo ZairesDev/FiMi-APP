0567220b5d56c400f1222e6eeb8e9ec11156ad4b
Chidori <business.faissal@outlook.com>
2022-04-14 01:04:16 -0500

    refactored signup page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,4 +1,17 @@
 import React, { useState } from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useMutation } from '@apollo/client';
 import { ADD_QASUP_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
@@ -30,41 +43,103 @@ const Signup = () => {
       console.error(err);
     }
   };
+
+  const theme = createTheme();
+
   // will need to look over relevant class name will depend on MUI library. Will modify during polishing tomorrow.
   return (
-    <main className=''>
-      <div className=''>
-        <div className=''>
-          <h3 className=''>Sign Up</h3>
-          <div className=''>
-            <form onSubmit={handleFormSubmit}>
-              <input
-                className='form-input'
-                placeholder='Your email'
-                name='email'
-                type='email'
-                id='email'
-                value={formState.email}
-                onChange={handleChange}
-              />
-              <input
-                className='form-input'
-                placeholder='******'
-                name='password'
-                type='password'
-                id='password'
-                value={formState.password}
-                onChange={handleChange}
-              />
-              <button className='' type='submit'>
-                Submit
-              </button>
-            </form>
-            {error && <div>Sign up failed</div>}
-          </div>
-        </div>
-      </div>
-    </main>
+    <ThemeProvider theme={theme}>
+      <Container component='main' maxWidth='xs'>
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component='h1' variant='h5'>
+            Sign up
+          </Typography>
+          <Box component='form' noValidate onSubmit={handleFormSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete='given-name'
+                  name='firstName'
+                  required
+                  fullWidth
+                  id='firstName'
+                  label='First Name'
+                  value={formState.firstName}
+                  onChange={handleChange}
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id='lastName'
+                  label='Last Name'
+                  name='lastName'
+                  autoComplete='family-name'
+                  value={formState.lastName}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id='email'
+                  label='Email Address'
+                  name='email'
+                  autoComplete='email'
+                  value={formState.email}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name='password'
+                  label='Password'
+                  type='password'
+                  id='password'
+                  autoComplete='new-password'
+                  value={formState.password}
+                  onChange={handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value='allowExtraEmails' color='primary' />}
+                  label='I want to receive inspiration, marketing promotions and updates via email.'
+                />
+              </Grid>
+            </Grid>
+            <Button type='submit' fullWidth variant='contained' sx={{ mt: 3, mb: 2 }}>
+              Sign Up
+            </Button>
+            <Grid container justifyContent='flex-end'>
+              <Grid item>
+                <Link href='#' variant='body2'>
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+            {error && <Box>Sign up failed</Box>}
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 5 }} />
+      </Container>
+    </ThemeProvider>
   );
 };
 
