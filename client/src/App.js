e32d8babb349a3323cc5e7685808ce3d953ff79c
ZairesDev <business.faissal@outlook.com>
2022-08-17 23:18:14 -0500

    setting up app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
-function App() {
+//import the different pages
+//TODO: create tests for every page
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import EmployeeSearch from './pages/EmployeeSearch';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const tokenId = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: tokenId ? `Bearer ${tokenId}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ApolloProvider client={client}></ApolloProvider>
     </div>
   );
-}
-
-export default App;
+};
